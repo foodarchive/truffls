2ccb17cf994414f9c7a9c3f5d4b9519d9127dbf0
=Sitaram J <=chaudhary201@gmail.com>
2020-07-09 16:24:07 +0700

    Go project setup

---
+++ cmd/truffls/main.go
@@ -0,0 +1,13 @@
+/*
+Copyright 2020 @truffls contributors.
+Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
+you may not use this file except in compliance with the License.
+*/
+
+package main
+
+import 	"fmt"
+
+func main() {
+	fmt.Println("starting...")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/foodarchive/truffls
+
+go 1.14
